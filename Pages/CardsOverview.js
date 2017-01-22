7857b95da51c15915b4d187f87f97b14eca0fa04
shrijo <Shrijo@home.home>
2017-01-22 10:42:52 +0100

    Added Creation Screen

---
+++ Pages/CardsOverview.js
@@ -0,0 +1,43 @@
+var Observable = require("FuseJS/Observable");
+
+var name = Observable();
+var description = Observable();
+var dbData = Observable();
+
+function goToCreateRecipe() {
+    router.push("createRecipe");
+};
+
+fetch('https://deels-e8257.firebaseio.com/recipes.json', {
+	method: 'GET',
+    cache: 'default',
+    headers: { "Content-type": "application/json"}
+})
+
+	.then(function(result) {
+		if (result.status !== 200) {
+			console.log("Something went wrong :(");
+			return;}
+		return result.json();
+	})
+            
+	.then(function(data) {
+		var keys = Object.keys(data);
+		keys.forEach(function(key, index) {
+			if (index >= dbData.length) {
+				var value = data[key];
+				dbData.add(value);
+			}
+		})
+	});
+
+
+
+
+module.exports = {
+	name: name,
+	description: description,
+	dbData: dbData,
+
+	goToCreateRecipe: goToCreateRecipe
+};
---
+++ Pages/CreateRecipe.js
@@ -0,0 +1,9 @@
+var Observable = require('FuseJS/Observable');
+
+function goBack() {
+    router.goBack();
+}
+
+module.exports = {
+	goBack: goBack
+};
