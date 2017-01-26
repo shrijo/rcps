dcdd6e97a914763af0d587705bc0a72e764d7f80
alex jenter <alexjenter@alexs-MacBook-Pro.local>
2017-01-26 21:16:21 +0100

    MOD: hooked up form submit to post to firebase

--- Pages/CreateRecipe.js
+++ Pages/CreateRecipe.js
@@ -1,9 +1,33 @@
 var Observable = require('FuseJS/Observable');
 
+var name = Observable();
+var description = Observable();
+
+function submit() {
+    fetch('https://deels-e8257.firebaseio.com/recipes.json', {
+        method: 'POST',
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ 
+            "name": name.value,
+            "description": description.value
+        })
+    });
+
+    // tear down
+    name.value = "";
+    description.value = "";
+    goBack();
+}
+
+
 function goBack() {
     router.goBack();
 }
 
 module.exports = {
-	goBack: goBack
+    name: name,
+    description: description,
+
+    goBack: goBack,
+    submit: submit
 };
