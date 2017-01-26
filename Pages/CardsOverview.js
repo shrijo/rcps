94940570b30db490ebd0eb9c3c4843547a0ee4a4
alex jenter <alexjenter@alexs-MacBook-Pro.local>
2017-01-26 21:15:29 +0100

    MOD: refactored fetch into function

--- Pages/CardsOverview.js
+++ Pages/CardsOverview.js
@@ -5,39 +5,42 @@ var description = Observable();
 var dbData = Observable();
 
 function goToCreateRecipe() {
-    router.push("createRecipe");
+	router.push("createRecipe");
 };
 
-fetch('https://deels-e8257.firebaseio.com/recipes.json', {
-	method: 'GET',
-    cache: 'default',
-    headers: { "Content-type": "application/json"}
-})
-
-	.then(function(result) {
-		if (result.status !== 200) {
-			console.log("Something went wrong :(");
-			return;}
-		return result.json();
+function fetchFromFirebase() {
+	fetch('https://deels-e8257.firebaseio.com/recipes.json', {
+		method: 'GET',
+		cache: 'default',
+		headers: { "Content-type": "application/json" }
 	})
-            
-	.then(function(data) {
-		var keys = Object.keys(data);
-		keys.forEach(function(key, index) {
-			if (index >= dbData.length) {
-				var value = data[key];
-				dbData.add(value);
+
+		.then(function (result) {
+			if (result.status !== 200) {
+				console.log("Something went wrong :(");
+				return;
 			}
+			return result.json();
 		})
-	});
 
+		.then(function (data) {
+			var keys = Object.keys(data);
+			keys.forEach(function (key, index) {
+				if (index >= dbData.length) {
+					var value = data[key];
+					dbData.add(value);
+				}
+			})
+		});
+}
 
-
+fetchFromFirebase();
 
 module.exports = {
 	name: name,
 	description: description,
 	dbData: dbData,
 
-	goToCreateRecipe: goToCreateRecipe
+	goToCreateRecipe: goToCreateRecipe,
+	fetchFromFirebase: fetchFromFirebase
 };
